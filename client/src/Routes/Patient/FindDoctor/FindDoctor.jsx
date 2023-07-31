620ac6025131dc4151a9d6ea25e7236c497fb1b9
Reese Chong <dev.reese.chong@gmail.com>
2023-07-31 16:28:34 -0400

    Style find doctor page

--- client/src/Routes/Patient/FindDoctor/FindDoctor.jsx
+++ client/src/Routes/Patient/FindDoctor/FindDoctor.jsx
@@ -1,8 +1,9 @@
 import { Box } from '@chakra-ui/react';
 import Navigation from '../../../Components/Navigation/Navigation';
 import Footer from '../../../Components/Footer/Footer';
-import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import { Image, Text, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+import './FindDoctor.scss';
 
 export default function FindDoctor() {
     // const DoctorSearchBar = ({ onSearch }) => {
@@ -22,16 +23,20 @@ export default function FindDoctor() {
     return (
         <Box>
             <Navigation paths={paths} />
-            <InputGroup>
-                <Input
-                type="text"
-                placeholder="Search doctors..."
-                // onChange={handleSearchChange}
-                />
-                <InputRightElement>
-                   <SearchIcon color="gray.500" />
-                 </InputRightElement>
-            </InputGroup>
+            <Box className="finddoctor-main">
+                <Text m='3' fontSize={'2xl'} fontWeight={''}>Discover Your Perfect Doctor in Your Area</Text>
+                <Text m='3' fontsize={'xs'}>Type in your desired specialty or doctor's name in the search bar above, and we'll display a list of relevant options. Let's find a suitable healthcare professional for you.</Text>
+                <InputGroup>
+                    <Input
+                    type="text"
+                    placeholder="Search doctors..."
+                    // onChange={handleSearchChange}
+                    />
+                    <InputRightElement>
+                    <SearchIcon color="gray.500" />
+                    </InputRightElement>
+                </InputGroup>
+            </Box>
             <Footer/>
         </Box>
     );
