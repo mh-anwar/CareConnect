a5e915248f24446f398d4d4a8968ba1a968d3459
mh-anwar <mohammad.anwar2939@gmail.com>
2023-07-31 13:35:42 -0400

    Add SyncFusion Calendar

--- client/src/Routes/HCP/Schedule/ManageSchedule.jsx
+++ client/src/Routes/HCP/Schedule/ManageSchedule.jsx
@@ -1,15 +1,31 @@
 import { Box } from '@chakra-ui/react';
 import {
     ScheduleComponent,
+    Day,
     Week,
+    WorkWeek,
     Month,
-    TimelineViews,
-    TimelineMonth,
+    Agenda,
     Inject,
-    ViewsDirective,
-    ViewDirective,
 } from '@syncfusion/ej2-react-schedule';
 
 export default function ManageSchedule() {
-    return <Box></Box>;
+    const data = [
+        {
+            Id: 1,
+            Subject: 'Meeting',
+            StartTime: new Date(2023, 1, 15, 10, 0),
+            EndTime: new Date(2023, 1, 15, 12, 30),
+        },
+    ];
+    return (
+        <ScheduleComponent
+            selectedDate={new Date(2023, 1, 15)}
+            eventSettings={{
+                dataSource: data,
+            }}
+        >
+            <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+        </ScheduleComponent>
+    );
 }
