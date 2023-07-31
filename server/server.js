4821a3f7f6897b459a9045c22ad7968d5db63457
mh-anwar <mohammad.anwar2939@gmail.com>
2023-07-31 02:09:47 -0400

    Re-organize server.js

--- server/server.js
+++ server/server.js
@@ -6,13 +6,6 @@ import PatientRouter from './patients/patientRoute.js';
 import ApptRouter from './appointments/apptRoute.js';
 import HPCRouter from './hpc/hpcRoute.js';
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.listen(port);
-
-app.use(helmet());
-
 const allowedOrigins = ['http://localhost:5173', undefined]; // TODO In prod, remove undefined (it lets Postman work)
 const corsOptions = {
     origin: (origin, callback) => {
@@ -25,6 +18,13 @@ const corsOptions = {
     },
 };
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+
+// Attach middleware
+app.use(helmet());
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
